c33a2223e0b58078510047198d9c46425efe1289
Phil Shapiro <philshap@gmail.com>
2025-12-01 22:02:44 -0600

    day 1

--- src/advent2025/Day1.java
+++ src/advent2025/Day1.java
@@ -5,21 +5,67 @@ public class Day1 extends Day {
     super(1);
   }
 
+  static int lineToOffset(String line) {
+    return (line.charAt(0) == 'R' ? 1 : -1) * Integer.parseInt(line.substring(1));
+  }
+
   String part1() {
-    return "0";
+    int dial = 50;
+    int numZeros = 0;
+    for (String line : input) {
+      dial += lineToOffset(line);
+      if (dial > 99) {
+        dial %= 100;
+      } else if (dial < 0) {
+        dial = 100 - (-dial % 100);
+      }
+      if (dial == 0 || dial == 100) {
+        numZeros++;
+      }
+    }
+    return String.valueOf(numZeros);
   }
 
   String part2() {
-    return "0";
+    int dial = 50;
+    int numZeros = 0;
+    for (String line : input) {
+      int offset = lineToOffset(line);
+      if (offset > 0) {
+        dial += offset;
+        numZeros += dial / 100;
+        dial %= 100;
+      } else {
+        while (offset++ != 0) {
+          if (--dial == 0) {
+            numZeros++;
+          }
+          if (dial == -1) {
+            dial = 99;
+          }
+        }
+      }
+    }
+    return String.valueOf(numZeros);
   }
 
   public static void main(String[] args) {
     Day day = new Day1() {
       @Override
       String getData() {
-        return "";
+        return """
+            L68
+            L30
+            R48
+            L5
+            R60
+            L55
+            L1
+            L99
+            R14
+            L82""";
       }
     };
-    day.run("0", "0");
+    day.run("3", "6");
   }
 }
