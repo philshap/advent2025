9163a859e5ddb0d1ba8168805658458500c82bf9
Phil Shapiro <philshap@gmail.com>
2025-12-02 23:44:34 -0600

    day 3

---
+++ src/advent2025/Day3.java
@@ -0,0 +1,51 @@
+package advent2025;
+
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+public class Day3 extends Day {
+  public Day3() {
+    super(3);
+  }
+
+  record Battery(int pos, int jolt) {}
+
+  long maxJoltage2(String bank, int digits) {
+    long joltage = 0;
+    int offset = 0;
+    for (int i = 0; i < digits; i++) {
+      Battery battery = IntStream.range(offset, bank.length() - (digits - i) + 1)
+          .mapToObj(pos -> new Battery(pos, bank.charAt(pos) - '0'))
+          .max(Comparator.comparing(Battery::jolt)).orElseThrow();
+      joltage = battery.jolt + joltage * 10;
+      offset = battery.pos + 1;
+    }
+    return joltage;
+  }
+
+  @Override
+  String part1() {
+    long total = input.stream().mapToLong(bank -> maxJoltage2(bank, 2)).sum();
+    return String.valueOf(total);
+  }
+
+  @Override
+  String part2() {
+    long total = input.stream().mapToLong(bank -> maxJoltage2(bank, 12)).sum();
+    return String.valueOf(total);
+  }
+
+  public static void main(String[] args) {
+    Day day = new Day3() {
+      @Override
+      String getData() {
+        return """
+            987654321111111
+            811111111111119
+            234234234234278
+            818181911112111""";
+      }
+    };
+    day.run("357", "3121910778619");
+  }
+}
