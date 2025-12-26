f4a30de3cada3dc6d3b95a4857d0914d873cec43
Phil Shapiro <philshap@gmail.com>
2025-12-25 19:46:43 -0500

    cleanups

--- src/advent2025/Support.java
+++ src/advent2025/Support.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
